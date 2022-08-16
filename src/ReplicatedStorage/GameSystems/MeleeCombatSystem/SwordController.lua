f67aed83a86be3588a7943892f8346d3c6bc9c4f
Christopher S <78832631+RecursiveMighty@users.noreply.github.com>
2022-08-15 22:14:14 -0400

    heirarchy setup

---
+++ src/ReplicatedStorage/GameSystems/MeleeCombatSystem/SwordController.lua
@@ -0,0 +1,18 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local SwordController = Knit.CreateController { Name = "SwordController" }
+
+
+function SwordController:KnitStart()
+    
+end
+
+
+function SwordController:KnitInit()
+    
+end
+
+
+return SwordController
