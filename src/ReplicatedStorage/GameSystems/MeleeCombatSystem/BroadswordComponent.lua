380a8ea79cb6876455e94ef1c062e3fbbc6fba1c
Christopher S <78832631+RecursiveMighty@users.noreply.github.com>
2022-08-15 22:08:11 -0400

    mcs setup

---
+++ src/ReplicatedStorage/GameSystems/MeleeCombatSystem/BroadswordComponent.lua
@@ -0,0 +1,19 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Packages.Component)
+
+local BroadswordComponent = Component.new({ Tag = "BroadswordComponent", Extensions = {} })
+
+function BroadswordComponent:Construct()
+
+end
+
+function BroadswordComponent:Start()
+
+end
+
+function BroadswordComponent:Stop()
+
+end
+
+return BroadswordComponent
---
+++ src/ServerScriptService/GameSystems/MeleeCombatSystem/MeleeCombatService.lua
@@ -0,0 +1,21 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local MeleeCombatService = Knit.CreateService {
+    Name = "MeleeCombatService";
+    Client = {};
+}
+
+
+function MeleeCombatService:KnitStart()
+    
+end
+
+
+function MeleeCombatService:KnitInit()
+    
+end
+
+
+return MeleeCombatService
